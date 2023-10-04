29510b9dd4f03b63c464401763a29c81f1fd5e8a
mohamad ali <mohammed.ali@kf.iq>
2023-10-04 15:48:11 +0300

    collection view

---
+++ test/FirstCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FirstCollectionViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 04/10/2023.
+//
+
+import UIKit
+
+class FirstCollectionViewCell: UICollectionViewCell {
+    
+}
