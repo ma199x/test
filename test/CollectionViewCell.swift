6434b8446c5bf4c7ddb46c36d90b7c85159d77e3
mohamad ali <mohammed.ali@kf.iq>
2023-10-04 16:38:17 +0300

    cv

---
+++ test/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 04/10/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ test/verticalcollectionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  verticalcollectionCollectionViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 04/10/2023.
+//
+
+import UIKit
+
+class verticalcollectionCollectionViewCell: UICollectionViewCell {
+    
+}
