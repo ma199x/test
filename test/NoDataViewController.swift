cab90b620ab48ac5721a39de6cfa3f443b6ea2c4
mohamad ali <mohammed.ali@kf.iq>
2023-10-04 11:40:22 +0300

    end tbv

--- test/NoDataViewController.swift
+++ test/NoDataViewController.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-class NoDataViewController: UIViewController {
-
+class NoDataViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource {
+    
+    var arr = ["1","2","3"]
+    @IBOutlet weak var tb:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tb.delegate = self
+        tb.dataSource = self
+       
+        arr.removeAll()
+        if arr.count == 0 {
+            tb.isHidden = true
+            let ImgError = UIImageView(frame: CGRect(x: 50,
+                                                     y: 100,
+                                                     width: self.view.frame.width - 100,
+                                                     height: self.view.frame.height - 200))
+            if let image =  UIImage(systemName: "heart")
+            {
+                ImgError.image = image
+                ImgError.tintColor = UIColor.gray
+                self.view.addSubview(ImgError)
+            }
+            else{
+                print("unable create ui image")
+            }
+            let Xpostion = (self.view.frame.width / 2 ) - (100 / 2)
+            let labMsg = UILabel(frame: CGRect(x:Xpostion ,
+                                               y: ImgError.frame.height + 50,
+                                               width: 100, height: 10))
+            labMsg.text = "ssdsd'sld;'sdl"
+            labMsg.backgroundColor = UIColor.red
+            self.view.addSubview(labMsg)
+            
+           
 
-        // Do any additional setup after loading the view.
+
+        }
+          
+      
+            
+        }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tb.dequeueReusableCell(withIdentifier: "cellName", for: indexPath)
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
