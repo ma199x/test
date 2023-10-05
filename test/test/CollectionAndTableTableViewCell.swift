0e7b459a8e66cac999cdd60ed56eb2202328bbb1
mohamad ali <mohammed.ali@kf.iq>
2023-10-05 16:53:30 +0300

    t

---
+++ test/test/CollectionAndTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionAndTableTableViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 05/10/2023.
+//
+
+import UIKit
+
+class CollectionAndTableTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellTBV: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ test/test/collectionAndTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  collectionAndTableViewController.swift
+//  test
+//
+//  Created by Mohammed Ali on 05/10/2023.
+//
+
+import UIKit
+
+class collectionAndTableViewController: UIViewController {
+
+    @IBOutlet weak var tableInView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
