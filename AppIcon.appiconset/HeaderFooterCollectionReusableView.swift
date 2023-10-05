882db6c5decd68207602a99e7eae451d1470aaaa
mohamad ali <mohammed.ali@kf.iq>
2023-10-05 15:28:45 +0300

    t

---
+++ AppIcon.appiconset/HeaderFooterCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderFooterCollectionReusableView.swift
+//  test
+//
+//  Created by Mohammed Ali on 05/10/2023.
+//
+
+import UIKit
+
+class HeaderFooterCollectionReusableView: UICollectionReusableView {
+        
+}
---
+++ test/HeaderFooterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderFooterCollectionViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 05/10/2023.
+//
+
+import UIKit
+
+class HeaderFooterCollectionViewCell: UICollectionViewCell {
+    
+}
