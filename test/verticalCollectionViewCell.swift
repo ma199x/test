e7182c0403b93a9608f82c98efbc6dbcde0fdacb
mohamad ali <mohammed.ali@kf.iq>
2023-10-05 12:51:50 +0300

    test

---
+++ test/verticalCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  verticalCollectionViewCell.swift
+//  test
+//
+//  Created by Mohammed Ali on 05/10/2023.
+//
+
+import UIKit
+
+class verticalCollectionViewCell: UICollectionViewCell {
+    
+}
